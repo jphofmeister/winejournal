7899a7cea4802a3cc8a81f22b03d7f6413dfcd76
Joseph Hofmeister <jphofmeister@gmail.com>
2018-11-23 19:56:57 -0500

    protect cloud keys

--- config/cloudinary-keys.js
+++ config/cloudinary-keys.js
@@ -1,16 +1,5 @@
-// const cloudinary = require('cloudinary');
-// const cloudinary-keys = require('./config/cloudinary-keys');
-// cloudinary.config(cloudinary-keys);
-
-
-// cloudinary.config({ 
-//   cloud_name: 'dila7a9z4', 
-//   api_key: '753439312426169', 
-//   api_secret: 'zhb2SIwTCbg29toUV3WWl4-HnRc' 
-// });
-
-module.exports = {
-  cloud_name: 'dila7a9z4',
-  api_key: '753439312426169',
-  api_secret: 'zhb2SIwTCbg29toUV3WWl4-HnRc'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./cloud_keys_prod');
+} else {
+  module.exports = require('./cloud_keys_dev');
 }
