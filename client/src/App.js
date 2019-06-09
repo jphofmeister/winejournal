7e75bfe2ea536981f384a72c5e51d7e005cd1808
Joe <jphofmeister@gmail.com>
2019-06-09 02:00:13 -0400

    update some landing page and login  styles

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import store from './store';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
-import Navbar from './components/layout/Navbar';
+// import Navbar from './components/layout/Navbar';
 // import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
@@ -51,7 +51,7 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div>
-            <Navbar />
+            {/* <Navbar /> */}
 
             <Route exact path="/" component={Landing} />
             <div>
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import wine_journal_logo from '../../images/wine_journal_logo.png'
 
 class Landing extends Component {
   componentDidMount() {
@@ -13,9 +14,13 @@ class Landing extends Component {
   render() {
     return (
       <section className="landing">
+        <img src={wine_journal_logo} />
         <div className="container">
-          <h1>Wine Journal</h1>
-          <p>Keep your wines here! Organize your wine cellar, write down notes, rate your favorites, and more!</p>
+          <h1>Record your<br />
+            Wine Discoveries</h1>
+          <p>
+            This wine journal allows you to create simple-to-detailed entries of your wine tastings. Upload a picture, enter basic details, write tasting notes, and more.
+          </p>
           <div className="landing-buttons">
             <Link to="/login" className="button">
               LOGIN
