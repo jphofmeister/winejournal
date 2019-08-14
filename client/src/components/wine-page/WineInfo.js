720d7c744bf454233cf2e9dc83d574fd1021b438
Joe <jphofmeister@gmail.com>
2019-08-13 23:58:21 -0400

    update wine info page styles

--- client/src/components/wine-page/WineInfo.js
+++ client/src/components/wine-page/WineInfo.js
@@ -26,6 +26,14 @@ class WineInfo extends Component {
       date = "-";
     }
 
+    const wineBackgroundImage = {
+      backgroundImage: `url(${wine.wineImageUrl})`,
+      backgroundAttachment: 'fixed',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'contain'
+    }
+
     return (
       <div className="wine-info">
 
@@ -35,30 +43,32 @@ class WineInfo extends Component {
 
         <div className="wine-heading-img-container">
           <div className="wine-img">
-            <img src={wine.wineImageUrl} alt="" />
+            <img src={wine.wineImageUrl} alt="" className="img" />
+            {/* <div className="img" style={wineBackgroundImage}></div> */}
           </div>
         </div>
 
-        <div className="wine-content">
-          <div className="wine-heading">
-            <h1>{wine.wineName}</h1>
-            <button onClick={this.handleToggleShowEditForm} className="edit-wine-button">Edit Wine</button>
+        <div className="wine-content-container">
+          <div className="wine-content">
+            <div className="wine-heading">
+              <h1>{wine.wineName}</h1>
+              <button onClick={this.handleToggleShowEditForm} className="edit-wine-button">Edit Wine</button>
+            </div>
+            <div className="description-row"><div className="description-label">Winery:</div> {wine.winery} </div>
+            <div className="description-row"><div className="description-label">Wine Type:</div> {wine.wineType} </div>
+            <div className="description-row"><div className="description-label">Notes:</div> {wine.notes} </div>
+            <div className="description-row"><div className="description-label">Varietal:</div> {wine.varietal} </div>
+            <div className="description-row"><div className="description-label">Tasted On:</div> {date}</div>
+            <div className="description-row"><div className="description-label">Location Tasted At:</div> {wine.tasteLocation}</div>
+            <div className="description-row"><div className="description-label">Rating:</div> {wine.rating} Stars</div>
+            <div className="description-row"><div className="description-label">Alcohol Content:</div> {wine.alcoholContent}%</div>
+            <div className="description-row"><div className="description-label">Price:</div> ${wine.price}</div>
+            <div className="description-row"><div className="description-label">Vintage:</div> {wine.vintage}</div>
+            <button className="delete" onClick={this.handleDeleteClick.bind(this, wine._id)}>
+              <i className="fas fa-times" /> Delete
+            </button>
           </div>
-          <div className="description-row"><div className="description-label">Winery:</div> {wine.winery} </div>
-          <div className="description-row"><div className="description-label">Wine Type:</div> {wine.wineType} </div>
-          <div className="description-row"><div className="description-label">Notes:</div> {wine.notes} </div>
-          <div className="description-row"><div className="description-label">Varietal:</div> {wine.varietal} </div>
-          <div className="description-row"><div className="description-label">Tasted On:</div> {date}</div>
-          <div className="description-row"><div className="description-label">Location Tasted At:</div> {wine.tasteLocation}</div>
-          <div className="description-row"><div className="description-label">Rating:</div> {wine.rating} Stars</div>
-          <div className="description-row"><div className="description-label">Alcohol Content:</div> {wine.alcoholContent}%</div>
-          <div className="description-row"><div className="description-label">Price:</div> ${wine.price}</div>
-          <div className="description-row"><div className="description-label">Vintage:</div> {wine.vintage}</div>
-          <button className="delete" onClick={this.handleDeleteClick.bind(this, wine._id)}>
-            <i className="fas fa-times" /> Delete
-          </button>
         </div>
-
       </div>
     )
   }
