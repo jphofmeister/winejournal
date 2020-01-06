46f58de7424874d40d1ee9ed39badf477c67172c
Joe <jphofmeister@gmail.com>
2020-01-05 23:36:57 -0500

    fix cloudinary keys

--- config/cloud_keys_prod.js
+++ config/cloud_keys_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  cloud_name: 'process.env.CLOUD_NAME',
-  upload_preset: 'process.env.UPLOAD_PRESET',
-  api_key: 'process.env.API_KEY',
-  api_secret: 'process.env.API_SECRET'
+  cloud_name: process.env.CLOUD_NAME,
+  upload_preset: process.env.UPLOAD_PRESET,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 }
