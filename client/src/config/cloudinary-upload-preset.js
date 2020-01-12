0ccbc07db3e3fa891c466775279da9fdcb9fe83e
Joe <jphofmeister@gmail.com>
2020-01-12 01:17:33 -0500

    see if upload image problem is solved

--- client/src/config/cloudinary-upload-preset.js
+++ client/src/config/cloudinary-upload-preset.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cloud_keys_prod');
-} else {
-  module.exports = require('./cloud_keys_dev');
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./cloud_keys_prod');
+// } else {
+//   module.exports = require('./cloud_keys_dev');
+// }
+
+module.exports = {
+  cloud_name: 'hvc1lwmvp',
+  upload_preset: 'skbgk2wg'
 }
--- config/cloud_keys_dev.js
+++ config/cloud_keys_dev.js
@@ -3,4 +3,4 @@ module.exports = {
   upload_preset: 'skbgk2wg',
   api_key: '639432167837748',
   api_secret: 'B8pDEM_JPuBBJNDuGos5gyE9s7Y'
-}
+};
