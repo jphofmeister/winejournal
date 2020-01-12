65593cc78a2eaa44e6790cadd9e333f4c4b70d3a
Joe <jphofmeister@gmail.com>
2020-01-11 23:59:38 -0500

    see if upload image problem is solved

--- client/src/config/cloud_keys_dev.js
+++ client/src/config/cloud_keys_dev.js
@@ -1,6 +1,4 @@
 module.exports = {
   cloud_name: 'dila7a9z4',
-  upload_preset: 'ruclsedy',
-  api_key: '753439312426169',
-  api_secret: 'zhb2SIwTCbg29toUV3WWl4-HnRc'
+  upload_preset: 'ruclsedy'
 }
