c0852f3f9a71f14aeb0a80caa3546ba12f934c73
Joe <jphofmeister@gmail.com>
2020-01-12 00:51:34 -0500

    see if upload image problem is solved

--- client/src/config/cloud_keys_prod.js
+++ client/src/config/cloud_keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  cloud_name: process.env.CLOUD_NAME2,
-  upload_preset: process.env.UPLOAD_PRESET2
+  cloud_name: 'process.env.CLOUD_NAME2',
+  upload_preset: 'process.env.UPLOAD_PRESET2'
 }
