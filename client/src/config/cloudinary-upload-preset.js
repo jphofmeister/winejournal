ac40bab652499ab5748122fce8305050dcf69c5a
Joe <jphofmeister@gmail.com>
2020-01-12 01:23:25 -0500

    see if upload image problem is solved

--- client/src/config/cloudinary-upload-preset.js
+++ client/src/config/cloudinary-upload-preset.js
@@ -4,7 +4,13 @@
 //   module.exports = require('./cloud_keys_dev');
 // }
 
-module.exports = {
-  cloud_name: 'hvc1lwmvp',
-  upload_preset: 'skbgk2wg'
+// module.exports = {
+//   cloud_name: 'hvc1lwmvp',
+//   upload_preset: 'skbgk2wg'
+// }
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  module.exports = require('./cloud_keys_dev');
+} else {
+  module.exports = require('./cloud_keys_prod');
 }
